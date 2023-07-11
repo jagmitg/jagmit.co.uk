0a011af1c5f71bab0b6876fedaa5c9ac31409531
Jagmit G <jagmitg@TCG04743.local>
2023-07-11 19:21:47 +0100

    Updated repos mjs

---
+++ scripts/repos.mjs
@@ -0,0 +1,43 @@
+import fetch from "node-fetch";
+import { writeFile, readdir, unlink } from "fs/promises";
+import path from "path";
+
+const targetFolder = "./src/content/repo"; // Define the target folder here
+
+function deleteAllMDFiles() {
+  return readdir(targetFolder).then((files) => {
+    const deletePromises = files
+      .filter((file) => file.endsWith(".mdx"))
+      .map((file) => unlink(path.join(targetFolder, file)));
+    return Promise.all(deletePromises);
+  });
+}
+
+function createMDFiles() {
+  fetch("https://api.github.com/users/jagmitg/repos")
+    .then((response) => response.json())
+    .then((data) => {
+      const writePromises = data
+        .filter((repo) => !repo.fork)
+        .map((repo) => {
+          let content = `---
+title: ${repo.name}
+date: ${new Date().toISOString().slice(0, 10)}
+repo: ${repo.html_url}
+---`;
+          return writeFile(
+            path.join(targetFolder, `${repo.name}.mdx`),
+            content
+          );
+        });
+      return Promise.all(writePromises);
+    })
+    .then(() =>
+      console.log("All markdown files have been created successfully.")
+    )
+    .catch((error) => console.error("An error occurred:", error));
+}
+
+deleteAllMDFiles()
+  .then(createMDFiles)
+  .catch((error) => console.error("An error occurred:", error));
