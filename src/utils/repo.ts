c030c311f97600a452e1c5cfe702baa223109fa3
Jagmit G <jagmitg@TCG04743.local>
2023-07-11 19:21:24 +0100

    Updated repo structure with randomised repos

---
+++ src/utils/repo.ts
@@ -0,0 +1,16 @@
+import { getCollection } from "astro:content";
+import type { CollectionEntry } from "astro:content";
+
+export type Collections = CollectionEntry<"repo">[];
+
+export async function getBlogs() {
+  const posts = await getCollection("repo");
+
+  return sortByDate(posts);
+}
+
+export function sortByDate(collections: Collections) {
+  return collections.sort(
+    (a, b) => new Date(b.data.date).getTime() - new Date(a.data.date).getTime()
+  );
+}
