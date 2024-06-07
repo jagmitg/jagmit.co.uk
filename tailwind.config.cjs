631aad94614fa80e8054137e5f32bd6f5e238e3c
Jagmit Gabba <jagmitg@gmail.com>
2024-06-07 23:21:21 +0100

    Updated readtime

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,43 +1,43 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  theme: {
-    fontFamily: {
-      body: [
-        "system-ui",
-        "Arial",
-        "Helvetica",
-        "'Hiragino Sans'",
-        "'Hiragino Kaku Gothic ProN'",
-        "'Meiryo,sans-serif'",
-        "sans-serif",
-      ],
-    },
-    listStyleType: {
-      none: "none",
-      hyphen: "'-  '",
-    },
-    extend: {
-      width: {
-        custom: "52rem",
-      },
-      colors: {
-        profile_blue: "#b9e1ff",
-      },
-      animation: {
-        "fade-in": "fadeIn 0.3s ease-in-out",
-      },
-      keyframes: {
-        fadeIn: {
-          "0%": {
-            opacity: 0,
-          },
-          "100%": {
-            opacity: 1,
-          },
-        },
-      },
-    },
-  },
-  plugins: [],
-};
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		fontFamily: {
+			body: [
+				'system-ui',
+				'Arial',
+				'Helvetica',
+				"'Hiragino Sans'",
+				"'Hiragino Kaku Gothic ProN'",
+				"'Meiryo,sans-serif'",
+				'sans-serif'
+			]
+		},
+		listStyleType: {
+			none: 'none',
+			hyphen: "'-  '"
+		},
+		extend: {
+			width: {
+				custom: '52rem'
+			},
+			colors: {
+				profile_blue: '#6bd3ff'
+			},
+			animation: {
+				'fade-in': 'fadeIn 0.3s ease-in-out'
+			},
+			keyframes: {
+				fadeIn: {
+					'0%': {
+						opacity: 0
+					},
+					'100%': {
+						opacity: 1
+					}
+				}
+			}
+		}
+	},
+	plugins: []
+}
