3b7f18a6b83ceec077da4686a53817edb5d4ccf6
Jagmit Gabba <jagmitg@gmail.com>
2024-12-28 22:23:23 +0000

    Update TypeScript configuration, improve reading time calculation, and remove deprecated blog entries

--- src/utils/readingTime.ts
+++ src/utils/readingTime.ts
@@ -1,4 +1,6 @@
 export function readingTime(html: string) {
+  if (!html) return '0 min read'
+
   const textOnly = html.replace(/<[^>]+>/g, "");
   const wordCount = textOnly.split(/\s+/).length;
   const readingTimeMinutes = ((wordCount / 200) + 1).toFixed();
