e074944f02c6ebe60001ce67838bc48cc5257d2a
Jagmit Gabba <jagmitg@gmail.com>
2025-01-03 21:18:07 +0000

    Updated readme times

--- src/utils/readingTime.ts
+++ src/utils/readingTime.ts
@@ -1,8 +1,10 @@
 export function readingTime(html: string) {
-  if (!html) return '0 min read'
+	if (!html) return '0 min read'
+	const textOnly = html.replace(/<[^>]+>/g, '').trim()
+	const wordCount = textOnly
+		.split(/\s+/)
+		.filter((word) => word.length > 0).length
 
-  const textOnly = html.replace(/<[^>]+>/g, "");
-  const wordCount = textOnly.split(/\s+/).length;
-  const readingTimeMinutes = ((wordCount / 200) + 1).toFixed();
-  return `${readingTimeMinutes} min read`;
+	const minutes = Math.ceil(wordCount / 200)
+	return `${minutes} min${minutes === 1 ? '' : 's'} read`
 }
